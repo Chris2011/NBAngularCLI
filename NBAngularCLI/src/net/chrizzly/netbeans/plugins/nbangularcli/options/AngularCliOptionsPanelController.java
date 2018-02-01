21c1a6a09e0dba57b53015efe3e34de70cde38e2
Chris2011 <christian.lenz@gmx.net>
2018-02-01 18:30:25 +0100

    Added option to set the AngularCLI globally.

---
+++ NBAngularCLI/src/net/chrizzly/netbeans/plugins/nbangularcli/options/AngularCliOptionsPanelController.java
@@ -0,0 +1,89 @@
+package net.chrizzly.netbeans.plugins.nbangularcli.options;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+@OptionsPanelController.SubRegistration(
+        location = "Html5",
+        displayName = "#AdvancedOption_DisplayName_NgCli",
+        keywords = "#AdvancedOption_Keywords_NgCli",
+        keywordsCategory = "Html5/AngularCLI"
+)
+@org.openide.util.NbBundle.Messages({"AdvancedOption_DisplayName_NgCli=Angular CLI", "AdvancedOption_Keywords_NgCli=Angular, Angular CLI"})
+public final class AngularCliOptionsPanelController extends OptionsPanelController {
+
+    private AngularCliPanel panel;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private boolean changed;
+
+    @Override
+    public void update() {
+        getPanel().load();
+        changed = false;
+    }
+
+    @Override
+    public void applyChanges() {
+        SwingUtilities.invokeLater(() -> {
+            getPanel().store();
+            changed = false;
+        });
+    }
+
+    @Override
+    public void cancel() {
+        // need not do anything special, if no changes have been persisted yet
+    }
+
+    @Override
+    public boolean isValid() {
+        return getPanel().valid();
+    }
+
+    @Override
+    public boolean isChanged() {
+        return changed;
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        return null; // new HelpCtx("...ID") if you have a help set
+    }
+
+    @Override
+    public JComponent getComponent(Lookup masterLookup) {
+        return getPanel();
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    private AngularCliPanel getPanel() {
+        if (panel == null) {
+            panel = new AngularCliPanel(this);
+        }
+        return panel;
+    }
+
+    void changed() {
+        if (!changed) {
+            changed = true;
+            pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+        }
+
+        pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+    }
+
+}
